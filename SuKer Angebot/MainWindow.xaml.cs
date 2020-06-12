141e9d1ddb273a7772756ea6492487fe468709aa
IT0Y-Funk <66824153+IT0Y-Funk@users.noreply.github.com>
2020-06-12 15:32:34 +0200

    Unnötige using-directives entfernt

--- SuKer Angebot/MainWindow.xaml.cs
+++ SuKer Angebot/MainWindow.xaml.cs
@@ -1,23 +1,6 @@
-﻿using SuKer_Angebot.Bindings;
-using SuKer_Angebot.Subwindows;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace SuKer_Angebot
 {
